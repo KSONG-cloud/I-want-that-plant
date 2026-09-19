e8342fc01efa0d9efd29af68b8e98bea53d2b1cc
Anthony <anthonywu26@gmail.com>
2026-09-19 17:23:44 +1000

    Database ER Diagram Initial Mockup
    - Created ER diagram from a database markup (.dbml) file using dbdiagram.io.
    - Generated .sql files for mySQL and Postgres.

---
+++ design_database/README.md
@@ -0,0 +1,11 @@
+
+# Database Design Work
+
+Description: Mockups and designs for the database.
+
+**Completed:**
+- Mockup ER diagram. Generated mock sql files.
+
+**TODO:**
+- Generate placeholder data records for testing.
+- Host the database locally using MySQL and test that it can be queried.
---
+++ design_database/mockup_postgres.sql
@@ -0,0 +1,49 @@
+CREATE TABLE "follows" (
+  "following_user_id" bigint,
+  "followed_user_id" bigint,
+  "created_at" timestamp NOT NULL,
+  PRIMARY KEY ("following_user_id", "followed_user_id")
+);
+
+CREATE TABLE "favorites" (
+  "listings_id" bigint,
+  "user_id" bigint,
+  "created_at" timestamp NOT NULL,
+  PRIMARY KEY ("user_id", "listings_id")
+);
+
+CREATE TABLE "users" (
+  "id" bigint PRIMARY KEY,
+  "username" varchar NOT NULL,
+  "password_hash" varchar NOT NULL,
+  "created_at" timestamp NOT NULL,
+  "updated_at" timestamp NOT NULL
+);
+
+CREATE TABLE "listings" (
+  "id" bigint PRIMARY KEY,
+  "name" varchar(255) NOT NULL,
+  "scientific_name" varchar(255),
+  "quantity" integer NOT NULL,
+  "address_line_1" varchar(255),
+  "address_line_2" varchar(255),
+  "suburb" varchar(100),
+  "state" varchar(100),
+  "postal_code" varchar(20),
+  "country_code" varchar(2),
+  "description" text,
+  "user_id" bigint NOT NULL,
+  "status" varchar(20) NOT NULL,
+  "created_at" timestamp NOT NULL,
+  "updated_at" timestamp NOT NULL
+);
+
+ALTER TABLE "favorites" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id") DEFERRABLE INITIALLY IMMEDIATE;
+
+ALTER TABLE "favorites" ADD FOREIGN KEY ("listings_id") REFERENCES "listings" ("id") DEFERRABLE INITIALLY IMMEDIATE;
+
+ALTER TABLE "listings" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id") DEFERRABLE INITIALLY IMMEDIATE;
+
+ALTER TABLE "follows" ADD FOREIGN KEY ("following_user_id") REFERENCES "users" ("id") DEFERRABLE INITIALLY IMMEDIATE;
+
+ALTER TABLE "follows" ADD FOREIGN KEY ("followed_user_id") REFERENCES "users" ("id") DEFERRABLE INITIALLY IMMEDIATE;
