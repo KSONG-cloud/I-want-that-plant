75e172c4a1a990ed34db071c87eb4563b4cff71a
Anthony <anthonywu26@gmail.com>
2026-09-23 14:52:45 +1000

    Update ER diagram with auto increment and default values
    - Primary key in users and listings tables now auto increment.
    - Datetime fields now autofill with current date when inserting new rows.

--- design_database/mockup_mysql.sql
+++ design_database/mockup_mysql.sql
@@ -1,27 +1,27 @@
 CREATE TABLE `follows` (
   `following_user_id` bigint,
   `followed_user_id` bigint,
-  `created_at` timestamp NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT (now()),
   PRIMARY KEY (`following_user_id`, `followed_user_id`)
 );
 
 CREATE TABLE `favorites` (
   `listings_id` bigint,
   `user_id` bigint,
-  `created_at` timestamp NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT (now()),
   PRIMARY KEY (`user_id`, `listings_id`)
 );
 
 CREATE TABLE `users` (
-  `id` bigint PRIMARY KEY,
+  `id` bigint PRIMARY KEY AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `password_hash` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL,
-  `updated_at` timestamp NOT NULL
+  `created_at` timestamp NOT NULL DEFAULT (now()),
+  `updated_at` timestamp NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE `listings` (
-  `id` bigint PRIMARY KEY,
+  `id` bigint PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `scientific_name` varchar(255),
   `quantity` integer NOT NULL,
@@ -33,9 +33,9 @@ CREATE TABLE `listings` (
   `country_code` varchar(2),
   `description` text,
   `user_id` bigint NOT NULL,
-  `status` varchar(20) NOT NULL,
-  `created_at` timestamp NOT NULL,
-  `updated_at` timestamp NOT NULL
+  `status` varchar(20) NOT NULL DEFAULT 'ACTIVE',
+  `created_at` timestamp NOT NULL DEFAULT (now()),
+  `updated_at` timestamp NOT NULL DEFAULT (now())
 );
 
 ALTER TABLE `favorites` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
